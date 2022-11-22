11d7bce918a4a227ab132b2d17177426063b9eb4
MuhammadWaqas4 <mwaqasbashir4@gmail.com>
2022-11-22 16:45:17 +0500

    dvcSDS

--- file.java
+++ file.java
@@ -1,23 +1,18 @@
 import java.util.Scanner;
 
-public class App {
-
-  public static Boolean items_validation(String inp) {
-    final String CODE_FOR_123 = "[1-9]";
-    Boolean validation = inp.matches(CODE_FOR_123);
-    return validation;
-  }
+public class file {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    // ! Input Validation code for integer :
+    final String CODE_FOR_ABC = "[a-zA-Z]";
+
     System.out.print("Enter the item_No you wanna Buy : ");
     String input = sc.next();
-    Boolean isValid = items_validation(input);
+    Boolean isValid = input.matches(CODE_FOR_ABC);
     while (isValid != true) {
       System.out.print("Enter Valid input : ");
       input = sc.next();
-      isValid = items_validation(input);
+      isValid = input.matches(CODE_FOR_ABC);
     }
     System.out.println("Your No is : " + input);
   }
